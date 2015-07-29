e8f20e4610c41ab8e7fd8e4260b9f9af3de916e6
sde <sde@localhost.localdomain>
2015-07-29 18:28:40 +0200

    commit angular

--- nodeJS_Test.js
+++ nodeJS_Test.js
@@ -2,6 +2,16 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Salut tout le monde !');
+  res.end('<html ng-app>
+<head>
+  <title>Hello World in AngularJS</title>
+</head>
+<body>
+
+<input ng-model="name"> Hello {{ name }}
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+</body>
+</html>');
 });
 server.listen(8080);
