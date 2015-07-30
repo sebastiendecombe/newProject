03b01a762577b3f7a4e52b51f75b311d3d190cb9
sebastiendecombe <sebastien.decombe@gmail.com>
2015-07-30 17:40:22 +0200

    ajout middelware e.favicon e.morgan

--- nodeJS_Test.js
+++ nodeJS_Test.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express();
 
 app.get('/etage/:etagenum/chambre', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
 });
 
 app.listen(8080);
